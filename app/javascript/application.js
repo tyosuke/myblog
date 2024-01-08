af0434ceaca459f7a40da072dae8bdd632b41f16
長留裕平 <tyosuke2010@yahoo.co.jp>
2024-01-08 13:23:06 +0900

    更新

---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+<a href="#" data-method="delete" data-confirm="Are you sure?" data-turbolinks="false">[DELETE]</a>
